2ad2fc0ffa56e25402068503f9508d0e0b7e3080
Luyss264 <joseacevedovargas@gmail.com>
2026-04-01 18:55:09 -0500

    modularizacion

---
+++ inputFunctions.py
@@ -0,0 +1,20 @@
+
+"""def id_student_function(students_list):
+    breaker = 0
+    while breaker != 1:
+
+        variable_input = input("\nWrite an ID of 4 digits for the student: ")
+
+        for i in students_list:
+            if variable_input = """
+
+
+def par_o_inpar():
+    input= int(input("ingrese el numero que desea consultar"))
+
+    if input % 2 == 0:
+        print('par')
+    else:
+        print('inpar')
+
+par_o_inpar()
